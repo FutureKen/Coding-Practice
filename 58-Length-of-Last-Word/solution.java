52e22014a2c390334680577bcf98dcabfbdf0d8c
FutureKen <zhaohaozhou@gmail.com>
2016-05-05 01:49:48 -0700

    [58. Length of Last Word][Accepted]committed by LeetCode Extension

--- 58-Length-of-Last-Word/solution.java
+++ 58-Length-of-Last-Word/solution.java
@@ -3,18 +3,18 @@ public class Solution {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        boolean islast = false;
         int count = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == ' ' && islast == true) {
-                break;
-            } else if (s.charAt(i) == ' ' && islast == false) {
-
-            } else {
-                if (islast == true) {
-                    count++;
+            if (count == 0) {
+                if (s.charAt(i) == ' ') {
+                    continue;
                 } else {
-                    islast = true;
+                    count++;
+                }
+            } else {
+                if (s.charAt(i) == ' ') {
+                    break;
+                } else {
                     count++;
                 }
             }
