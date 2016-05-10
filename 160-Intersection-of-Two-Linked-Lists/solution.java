ac3d5b6f1df86c6ddc277cca6fa8be9aa0a55395
FutureKen <zhaohaozhou@gmail.com>
2016-05-10 02:19:15 -0700

    [160. Intersection of Two Linked Lists][Accepted]committed by LeetCode Extension

---
+++ 160-Intersection-of-Two-Linked-Lists/solution.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        int lengA = getLength(headA);
+        int lengB = getLength(headB);
+        if (lengA >= lengB) {
+            while (lengA > lengB) {
+                headA = headA.next;
+                lengA--;
+            }
+        } else {
+            while (lengA < lengB) {
+                headB = headB.next;
+                lengB--;
+            }
+        }
+        while (headA != null) {
+            if (headA == headB) {
+                return headA;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+    }
+
+    public int getLength(ListNode head) {
+        int count = 0;
+        while (head != null) {
+            head = head.next;
+            count++;
+        }
+        return count;
+    }
+}
