04c47ba2fd25a330ed43b8b503190f3a98369775
FutureKen <zhaohaozhou@gmail.com>
2016-06-06 22:43:59 -0700

    [344. Reverse String][Accepted]committed by LeetCode Extension

---
+++ 344-Reverse-String/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String reverseString(String s) {
+        char[] str = s.toCharArray();
+        int i = 0;
+        int j = str.length - 1;
+        while (i < j) {
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(str);
+    }
+}
