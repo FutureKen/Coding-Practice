677bc70e57222a4dfe4c8758af02a43854292b3d
FutureKen <zhaohaozhou@gmail.com>
2016-08-18 22:59:15 -0700

    [107. Binary Tree Level Order Traversal II][Accepted]committed by LeetCode Extension

---
+++ 107-Binary-Tree-Level-Order-Traversal-II/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            Integer size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                level.add(node.val);
+            }
+            result.add(0,level);
+        }
+        return result;
+    }
+}
