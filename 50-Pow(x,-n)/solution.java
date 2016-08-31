d34dab7f82814f0ff0f0226a33acd1e44bc3c8c7
FutureKen <zhaohaozhou@gmail.com>
2016-08-31 04:09:31 -0700

    [50. Pow(x, n)][Accepted]committed by Ken

---
+++ 50-Pow(x,-n)/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == Integer.MIN_VALUE) {
+            n++;
+            x = x*x;
+        }
+        if (n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+        
+        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2); 
+    }
+}
