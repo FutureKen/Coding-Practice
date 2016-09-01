afc22366f6b29d6b87f7d1184627a9ae03b69904
FutureKen <zhaohaozhou@gmail.com>
2016-08-31 23:20:50 -0700

    [69. Sqrt(x)][Accepted]committed by Ken

---
+++ 69-Sqrt(x)/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int mySqrt(int x) {
+        if (x <= 1) {
+            return x;
+        }
+        int left = 1;
+        int right = x;
+        
+        while (left < right) {
+            int mid = right - (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
