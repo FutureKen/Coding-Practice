980d9d6d4b3f7f03273b239a287c9487355f04c0
FutureKen <zhaohaozhou@gmail.com>
2016-09-07 01:05:16 -0700

    [268. Missing Number][Accepted]committed by Ken

---
+++ 268-Missing-Number/solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        int i = 0;
+        for (; i < nums.length; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+        return xor ^ i;
+    }
+}
