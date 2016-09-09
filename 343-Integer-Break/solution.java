29457fc7d09fed95ac513c0149d623eb81b0ffe1
FutureKen <zhaohaozhou@gmail.com>
2016-09-09 02:52:35 -0700

    [343. Integer Break][Accepted]committed by Ken

---
+++ 343-Integer-Break/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int integerBreak(int n) {
+        int result = 1;
+        if (n == 2) {
+            return 1;
+        }
+        if (n == 3) {
+            return 2;
+        }
+        while (n > 4) {
+            result *= 3;
+            n -= 3;
+        }
+        result *= n;
+        return result;
+    }
+}
