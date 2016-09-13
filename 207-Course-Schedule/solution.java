6b2e05d49f1dcbf642a1bdbec7dc1eb234c3c490
FutureKen <zhaohaozhou@gmail.com>
2016-09-13 00:42:24 -0700

    [207. Course Schedule][Accepted]committed by Ken

---
+++ 207-Course-Schedule/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        for (int[] pair : prerequisites) {
+            indegree[pair[1]]++;
+        }
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            numCourses--;
+            int course = queue.poll();
+            for (int[] pair: prerequisites) {
+                if (pair[0] == course) {
+                    if (--indegree[pair[1]] == 0) {
+                        queue.offer(pair[1]);
+                    }
+                }
+            }
+        }
+        return numCourses == 0;
+    }
+}
