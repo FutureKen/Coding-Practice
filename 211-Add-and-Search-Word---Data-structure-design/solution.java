e2afc7de8839247adcb917cdc852cd04945f8e22
FutureKen <zhaohaozhou@gmail.com>
2016-09-21 02:46:34 -0700

    [211. Add and Search Word - Data structure design][Accepted]committed by Ken

--- 211-Add-and-Search-Word---Data-structure-design/solution.java
+++ 211-Add-and-Search-Word---Data-structure-design/solution.java
@@ -1,62 +1,63 @@
+
 class TrieNode {
 
-    public TrieNode[] children;
-    public boolean hasWord;
-    
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+    // Initialize your data structure here.
     public TrieNode() {
-        children = new TrieNode[26];
-        for (int i = 0; i < 26; ++i)
-            children[i] = null;
-        hasWord = false;
+
     }
 }
 
-
 public class WordDictionary {
     private TrieNode root;
- 
-    public WordDictionary(){
+
+    public WordDictionary() {
         root = new TrieNode();
     }
- 
+
     // Adds a word into the data structure.
     public void addWord(String word) {
-        // Write your code here
-        TrieNode now = root;
-        for(int i = 0; i < word.length(); i++) {
-            Character c = word.charAt(i);
-            if (now.children[c - 'a'] == null) {
-                now.children[c - 'a'] = new TrieNode();
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
             }
-            now = now.children[c - 'a'];
+            node = node.children[c - 'a'];
         }
-        now.hasWord = true;
+        node.isWord = true;
     }
-    
-    boolean find(String word, int index, TrieNode now) {
-        if(index == word.length()) {
-            return now.hasWord;
+
+    // Returns if the word is in the data structure. A word could
+    // contain the dot character '.' to represent any one letter.
+    public boolean search(String word) {
+        return find(word, 0, root);
+    }
+
+    public boolean find(String word, int index, TrieNode node) {
+        if (index == word.length()) {
+            return node.isWord;
         }
-        
-        Character c = word.charAt(index);
+        char c = word.charAt(index);
         if (c == '.') {
-            for(int i = 0; i < 26; ++i) 
-            if (now.children[i] != null) {
-                if (find(word, index+1, now.children[i]))
-                    return true;
+            for (int i = 0; i < 26; i++) {
+                if (node.children[i] != null) {
+                    if (find(word, index + 1, node.children[i])) {
+                        return true;
+                    }
+                }
             }
             return false;
-        } else if (now.children[c - 'a'] != null) {
-            return find(word, index+1, now.children[c - 'a']);  
+        } else if (node.children[c - 'a'] != null) {
+            return find(word, index + 1, node.children[c - 'a']);
         } else {
             return false;
         }
     }
-    
-    // Returns if the word is in the data structure. A word could
-    // contain the dot character '.' to represent any one letter.
-    public boolean search(String word) {
-        // Write your code here
-        return find(word, 0, root);
-    }
 }
+
+// Your WordDictionary object will be instantiated and called as such:
+// WordDictionary wordDictionary = new WordDictionary();
+// wordDictionary.addWord("word");
+// wordDictionary.search("pattern");
