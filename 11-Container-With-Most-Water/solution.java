bcf8466d0f2fdbba2220b1c74da4799c094bbc09
FutureKen <zhaohaozhou@gmail.com>
2016-09-27 02:04:25 -0700

    [11. Container With Most Water][Accepted]committed by Ken

--- 11-Container-With-Most-Water/solution.java
+++ 11-Container-With-Most-Water/solution.java
@@ -3,10 +3,7 @@ public class Solution {
         int left = 0, right = height.length - 1;
         int maxA = 0;
         while (left < right) {
-            int curA = Math.min(height[left], height[right]) * (right - left);
-            if (maxA < curA) {
-                maxA = curA;
-            }
+            maxA = Math.max(Math.min(height[left], height[right]) * (right - left), maxA);
             if (height[left] < height[right]) {
                 left++;
             } else {
