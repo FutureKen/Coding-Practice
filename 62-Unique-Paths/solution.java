9a1caa6a145bc8ff46735967ebea0b8f3c94e05b
FutureKen <zhaohaozhou@gmail.com>
2016-10-03 19:41:26 -0700

    [62. Unique Paths][Accepted]committed by Ken

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -1,20 +1,20 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
         if (m == 0 || n == 0) {
-        	return 0;
+            return 0;
         }
-        int[][] matrix = new int[m][n];
-        for (int i = 0; i < m; i++) {
-        	matrix[i][0] = 1;
-        }
+        int[][] res = new int[m][n];
         for (int i = 0; i < n; i++) {
-        	matrix[0][i] = 1;
+            res[0][i] = 1;
         }
-        for (int i = 1; i < m; i++) {
-        	for (int j = 1; j < n; j++) {
-        		matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1]; 
-        	}
+        for (int j = 1; j < m; j++) {
+            res[j][0] = 1;
         }
-        return matrix[m - 1][n - 1];
+        for (int j = 1; j < m; j++) {
+            for (int i = 1; i < n; i++) {
+                res[j][i] = res[j - 1][i] + res[j][i - 1];
+            }
+        }
+        return res[m - 1][n - 1];
     }
 }
