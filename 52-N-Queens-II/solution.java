5f4bf92e05507b85d6fa3acc194b4abaf6118dbc
FutureKen <zhaohaozhou@gmail.com>
2016-10-11 03:09:41 -0700

    [52. N-Queens II]committed by Ken

---
+++ 52-N-Queens-II/solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    int res;
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                board[i][j] = '.';
+        res = 0;
+        dfs(board, 0);
+        return res;
+    }
+    
+    private void dfs(char[][] board, int colIndex) {
+        if(colIndex == board.length) {
+            res++;
+            return;
+        }
+        
+        for(int i = 0; i < board.length; i++) {
+            if(validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                dfs(board, colIndex + 1);
+                board[i][colIndex] = '.';
+            }
+        }
+    }
+    
+    private boolean validate(char[][] board, int x, int y) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < y; j++) {
+                if(board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i))
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+    
+}
