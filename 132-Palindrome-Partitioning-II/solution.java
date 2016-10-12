e958ca4c8773aabf835eb31ea8c34c6ec1a3f4e9
FutureKen <zhaohaozhou@gmail.com>
2016-10-12 03:57:39 -0700

    [132. Palindrome Partitioning II]committed by Ken

---
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        int[] cut = new int[n];
+        boolean[][] pal = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(i) == s.charAt(j) && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
+                    pal[j][i] = true;
+                    min = j == 0 ? 0 : Math.min(min, cut[j - 1] + 1);
+                }
+            }
+            cut[i] = min;
+        }
+        return cut[n - 1];
+    }
+}
+/* cut[i] is the minimum of cut[j - 1] + 1 (j <= i), if [j, i] is palindrome.
+If [j, i] is palindrome, [j + 1, i - 1] is palindrome, and c[j] == c[i].
+*/
